20787427a8d2ffeac9c00c07084f2aab39dfcd2f
ulmu0426 <80729771+ulmu0426@users.noreply.github.com>
2023-07-28 23:04:54 +0900

    가독성을 위해 enter 구분

--- src/main/java/com/ulmu/dadjoketest/domain/Comment.java
+++ src/main/java/com/ulmu/dadjoketest/domain/Comment.java
@@ -16,23 +16,23 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id", unique = true, nullable = false)
     private Long commentId; //pk
-
+    
     @Column(name = "comment_detail", unique = false, nullable = false)
     private String commentDetail;  //댓글내용
-
+    
     @Column(name = "created_at", unique = false, nullable = false)
     @CreationTimestamp
     private Date createdAt;    //댓글 작성일
-
+    
     @Column(name = "great", unique = false, nullable = false)
     private Long great;  //댓글 좋아요 수
     
     @Column(name = "joke_id", unique = false, nullable = false)
     private Long jokeId; //댓글을 단 농담 id
-
+    
     @Column(name = "create_user_id", unique = false, nullable = false)
     private Long createUserId; //댓글 작성자 id
-
+    
     @Column(name = "comment_great_user_list", unique = false, nullable = false)
     private String greatUserIdList;     //좋아요 누른 유저 id list - 띄어쓰기로 구분해서 데이터 입력
                                         //DTO에서 Mapper가 List<String>을 ','로 구분되는 String으로 변환하여 통으로 저장하도록
