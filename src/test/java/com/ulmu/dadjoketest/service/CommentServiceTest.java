9430cabea41dfec0df05e8bfb97ed727d113c9eb
ulmu0426 <80729771+ulmu0426@users.noreply.github.com>
2023-08-02 18:02:57 +0900

    CommentService deleteComment Test Code 작성
    Test Code 정상 구현 완료

--- src/test/java/com/ulmu/dadjoketest/service/CommentServiceTest.java
+++ src/test/java/com/ulmu/dadjoketest/service/CommentServiceTest.java
@@ -1,5 +1,6 @@
 package com.ulmu.dadjoketest.service;
 
+import com.ulmu.dadjoketest.domain.Comment;
 import com.ulmu.dadjoketest.dto.CommentDto;
 import com.ulmu.dadjoketest.repository.CommentRepository;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
@@ -31,5 +35,30 @@ class CommentServiceTest {
         assertEquals(123L,commentDto.getCreateUserId());
         assertEquals(11L,commentDto.getJokeId());
         System.out.println(commentDto.getCommentId());
+    }
+
+    @Test
+    void deleteComment() {
+        Comment comment = new Comment();
+        comment.setCommentDetail("삭제 내용");
+        comment.setCreateUserId(123L);
+        comment.setJokeId(11L);
+
+        Comment savedComment = this.commentRepository.save(comment);
+
+        CommentDto deleteCommentDto = commentService.getComment(savedComment.getCommentId());
+        Long commentId = deleteCommentDto.getCommentId();
+        commentService.deleteComment(commentId);
+
+        Optional<Comment> res = commentRepository.findById(commentId);
+        assertTrue(res.isEmpty());
+    }
+
+    @Test
+    void putComment() {
+    }
+
+    @Test
+    void greatComment() {
     }
 }
