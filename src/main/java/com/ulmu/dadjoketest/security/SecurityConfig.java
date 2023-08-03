c6c597ff0a52d79b82e6cc44db23abee8c6d7770
ulmu0426 <80729771+ulmu0426@users.noreply.github.com>
2023-08-03 23:35:51 +0900

    spring Security 설정 중

---
+++ src/main/java/com/ulmu/dadjoketest/security/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.ulmu.dadjoketest.security;
+
+import com.ulmu.dadjoketest.domain.User;
+import com.ulmu.dadjoketest.repository.UserRepository;
+import com.ulmu.dadjoketest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Optional;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements UserDetailsService{
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //접근권한 설정
+        http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN");     //관리자 페이지, 해당 권한이 있으면 허용
+        http.authorizeHttpRequests().antMatchers("/member/**").hasAnyRole("ADMIN", "MEMBER");   //관리자, 맴버 역할 중 하나만 있으면 허용
+
+        http.authorizeHttpRequests().anyRequest().permitAll();  //임시로 모든 요청 허용
+
+        //사이트 위변조 요청 방지
+        http.csrf().disable();
+
+        //로그인 설정
+        http.formLogin()
+                .loginPage("/login")        //로그인페이지
+                .loginProcessingUrl("/login-process")   //submit 요청을 받을 url
+                .defaultSuccessUrl("/")     //로그인 성공시 기본페이지로
+                .failureUrl("/login")    //로그인 실패시 다시 로그인 페이지로
+                .usernameParameter("userId")
+                .passwordParameter("password");
+
+        //로그아웃 설정
+        http.logout()
+                .invalidateHttpSession(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user2/logout"));
+
+        //사용자 인증 처리 컴포넌트 서비스 등록
+        http.userDetailsService((UserDetailsService) userService);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new UserPasswordEncoder();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> res = userRepository.findByName(username);
+        User user = res.orElseThrow(() -> new UsernameNotFoundException("회원을 찾을 수 없습니다."));
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
---
+++ src/main/java/com/ulmu/dadjoketest/security/UserPasswordEncoder.java
@@ -0,0 +1,15 @@
+package com.ulmu.dadjoketest.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserPasswordEncoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return rawPassword.toString();
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encodedPassword.equals(encode(rawPassword));
+    }
+}
